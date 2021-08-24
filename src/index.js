1fe5c8fbec29be08b3347ee3a3a15b4e64481e6e
root <root@DESKTOP-T10Q99V.localdomain>
2021-08-23 21:52:55 -0500

    sample book layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// CSS
+import "./index.css";
+
+//stateless functional component
+//always return JSX
+//return single element
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/71bLle0wLjS._AC_UY218_.jpg"
+    alt=""
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>Quantum Physics for Beginners</h1>;
+
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.90rem", marginTop: "0.25rem" }}>
+    Carl J. Pratt
+  </h4>
+);
+
+//looking for what we are going to render and where to render its
+ReactDOM.render(<BookList />, document.getElementById("root"));
